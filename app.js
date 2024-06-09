1646930ded63559eee1273827c78e6ff4f6d1553
Mishu5 <michalgostkow@gmail.com>
2024-06-09 21:41:36 +0200

    .

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require("dotenv");
 
 var googleLoginRouter = require('./routes/google_login');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(dotenv.config());
 
 app.use('/', indexRouter);
 app.use('/google', googleLoginRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var {Client} = require("pg");
 
 var i = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+  const connectDb = async () => {
+    try{
+        const client = new Client({
+          user: process.env.PGUSER,
+          host: process.env.PGHOST,
+          database: process.env.PGDATABASE,
+          password: process.env.PGPASSWORD,
+          port: process.env.PGPORT
+        })
+        await client.connect()
+        const res = await client.query('SELECT * FROM users')
+        console.log(res)
+        await client.end()
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   res.render('index', {title: "google"});
 });
 
